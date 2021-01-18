da97f8fa4fedcbc37051fffb0548c316e9b2f678
diogobarrel <santos.barrel@gmail.com>
2021-01-18 20:51:02 -0300

    chore: chorando se foi

--- src/aed/matrix/Sparse2DMatrix.java
+++ src/aed/matrix/Sparse2DMatrix.java
@@ -1,126 +1,68 @@
 package aed.matrix;
+
 import aed.tables.OpenAddressingHashTable;
 
 @SuppressWarnings("unchecked")
 public class Sparse2DMatrix {
 
-
     int size;
-    Sparse2DMatrix[][] newMatrix;
-    Sparse2DMatrix matrixData;
     int numberOfColumns;
     int numberOfRows;
-
-    //put valid indexes in an nested array to easier access by hashing
-    public OpenAddressingHashTable hashTableMatrix;
-
-    public Sparse2DMatrix(){
-        this.matrixData = new Sparse2DMatrix();
-        this.hashTableMatrix = new OpenAddressingHashTable();
-        this.size = hashTableMatrix.size();
-    }
+    public OpenAddressingHashTable<Integer, Float> dataHashTable;
 
 
-
-    public Sparse2DMatrix(int lines, int columns)
-    {
-        this.newMatrix = new Sparse2DMatrix[lines][columns];
+    public Sparse2DMatrix(int lines, int columns) {
         this.numberOfRows = lines;
         this.numberOfColumns = columns;
-        new Sparse2DMatrix();
+        this.dataHashTable = new OpenAddressingHashTable<Integer, Float>();
+        this.size = dataHashTable.size();
+    }
+
+    // ----------------------------------------accessory methods;
+    private int hashingFuction(int line, int column, int numberOfColumns) {
+        int i = line * numberOfColumns + column;
+        return i;
     }
 
     public int getNumberNonZero() {
         return size;
     }
 
-    public void put(int line, int column, float value)
-    {
-        int matrixIndex = hashingIndex(line,column, numberOfColumns);
-        if (value == 0){
-            hashTableMatrix.put(matrixIndex,null);
+    public void put(int line, int column, float value) {
+        int matrixIndex = hashingFuction(line, column, numberOfColumns);
+        if (value == 0) {
+            dataHashTable.put(matrixIndex, null);
             return;
         }
-        hashTableMatrix.put(matrixIndex,value);
 
-
+        dataHashTable.put(matrixIndex, value);
     }
 
-    public float get(int line, int column)
-    {
-        int i = hashingIndex(line, column, numberOfColumns);
-        float value = (float) hashTableMatrix.get(i);
+    public float get(int line, int column) {
+        int hash = hashingFuction(line, column, numberOfColumns);
+        float value = (float) dataHashTable.get(hash);
         return value;
     }
 
-    public Sparse2DMatrix scalar(float scalar)
-    {
-        Iterable nextIterator = hashTableMatrix.keys();
-        for( Object v : hashTableMatrix.values){
-            float result = (float) v*scalar;
-            v = result;
-
+    public Sparse2DMatrix scalar(float scalar) {
+        Iterable tableIterator = dataHashTable.keys();
+        for (float v : dataHashTable.values) {
+            dataHashTable.put();
         }
-        Sparse2DMatrix multipliedMatrix = new Sparse2DMatrix();
-        multipliedMatrix.matrixData = this.matrixData;
-
-        return multipliedMatrix;
-
+        return this;
     }
 
-    public Sparse2DMatrix sum(Sparse2DMatrix that)
-    {
-
-        Sparse2DMatrix sumMatrixResult = new Sparse2DMatrix();
-            Iterable matrixAIterable = matrixData.hashTableMatrix.keys();
-            Iterable matrixThatIterable = that.hashTableMatrix.keys();
-        for (int i = 0; i < size; i++){
-            int keyA = (int) matrixAIterable.iterator().next();
-            float valueA = (float) this.matrixData.hashTableMatrix.get(keyA);
-            int keyThat = (int) matrixThatIterable.iterator().next();
-            float valueThat = (float) that.hashTableMatrix.get(keyThat);
-            float resultValue = valueA + valueThat;
-            sumMatrixResult.hashTableMatrix.put(keyA, resultValue);
+    public Sparse2DMatrix multiply(Sparse2DMatrix that) {
+        return this;
     }
 
-        return sumMatrixResult;
+    public int getNonZeroElements() {
+       return 3;
     }
 
-    public Sparse2DMatrix multiply(Sparse2DMatrix that)
-    {
-		return matrixData;
-    }
-	
-	public float[] getNonZeroElements()
-    {
-        float[] nonZeroArray = new float[size];
-        Iterable validKeysIterator = matrixData.hashTableMatrix.keys();
-
-        for (int i = 0;  i < size; i++ ){
-            int currentKey = (int) validKeysIterator.iterator().next();
-
-        }
-        return nonZeroArray;
-    }
-
-
-    public float[][] getNonSparseMatrix()
-    {
+    public float[][] getNonSparseMatrix() {
         float[][] sahud = new float[5][5];
         return sahud;
     }
-//  ----------------------------------------accessory methods;
-    public int hashingIndex(int line, int column, int numberOfColumns){
-        int i = line * numberOfColumns + column;
-        return i;
-    }
+
 }
-/*
-
-catch rows and columns reference from a ordinal indexes
-indexNumber = n
-columnsPosition = n % totalColumns;
-rowsPosition = n / totalColumns;
-
-matrixPosition = (rowsPosition, columnsPosition);
- */
--- src/aed/tables/OpenAddressingHashTable.java
+++ src/aed/tables/OpenAddressingHashTable.java
@@ -5,14 +5,9 @@ import java.util.Iterator;
 @SuppressWarnings("unchecked")
 public class OpenAddressingHashTable<Key, Value> {
     private static final int MIN_PRIMEINDEX = 1;
-    private static final int[] primes = {
-            17, 37, 79, 163, 331,
-            673, 1361, 2729, 5471, 10949,
-            21911, 43853, 87719, 175447, 350899,
-            701819, 1403641, 2807303, 5614657,
-            11229331, 22458671, 44917381, 89834777, 179669557
-    };
-    //public for test propouses. main_hashTable must be able to access this arrays;
+    private static final int[] primes = { 17, 37, 79, 163, 331, 673, 1361, 2729, 5471, 10949, 21911, 43853, 87719,
+            175447, 350899, 701819, 1403641, 2807303, 5614657, 11229331, 22458671, 44917381, 89834777, 179669557 };
+    // public for test propouses. main_hashTable must be able to access this arrays;
     public Key[] keys;
     public Value[] values;
     int m;
@@ -22,9 +17,9 @@ public class OpenAddressingHashTable<Key, Value> {
 
     @SuppressWarnings("unchecked")
     public OpenAddressingHashTable() {
-        //Keys e Values são arrays com index correspondentes;
-        //m = lenght dos arrays Keys e Values;
-        //n = número de elementos nestes arrays;
+        // Keys e Values são arrays com index correspondentes;
+        // m = lenght dos arrays Keys e Values;
+        // n = número de elementos nestes arrays;
 
         this.size = 0;
         this.primeIndex = 1;
@@ -61,19 +56,21 @@ public class OpenAddressingHashTable<Key, Value> {
     }
 
     private int hash(Key k) {
-        //using hashCode with an function that convert negative
-        //values into positive. Otherway, it would generate negative
-        //indexes;
-        //% operator guarantee loop over array when hashCode is bigger
-        //than array lenght (m);
+        // using hashCode with an function that convert negative
+        // values into positive. Otherway, it would generate negative
+        // indexes;
+        // % operator guarantee loop over array when hashCode is bigger
+        // than array lenght (m);
         return (k.hashCode() & 0x7fffffff) % m;
     }
 
     private int doubleHash(Key k) {
         int p = primes[primeIndex - 1];
         return p - ((k.hashCode() & 0x7fffffff) % p);
-        //um methodo de evitar isso é criar um segundo hashing, idependente do primeiro, que possa fazer iterações saltando
-        //entre keys, mas sem causar aglomeração uma vez que usa a mesma lógica do hash. Tende a manter distribuição mais uniforme;
+        // um methodo de evitar isso é criar um segundo hashing, idependente do
+        // primeiro, que possa fazer iterações saltando
+        // entre keys, mas sem causar aglomeração uma vez que usa a mesma lógica do
+        // hash. Tende a manter distribuição mais uniforme;
     }
 
     public boolean containsKey(Key k) {
@@ -82,7 +79,8 @@ public class OpenAddressingHashTable<Key, Value> {
         int initial = i;
 
         for (int j = 0; keys[i] != null; i = (initial + (y * ++j)) % m)
-            if (keys[i].equals(k) && values[i] != null) return true;
+            if (keys[i].equals(k) && values[i] != null)
+                return true;
         return false;
     }
 
@@ -91,10 +89,11 @@ public class OpenAddressingHashTable<Key, Value> {
         int y = doubleHash(k);
         int initial = i;
 
-        //difference between == and .equals():
+        // difference between == and .equals():
         // == operator do an adress comparation, setted in object memory location;
-        //.equals() method compares the content. Even if the objects has different memory adress location, if the content
-        //is equal, it return true
+        // .equals() method compares the content. Even if the objects has different
+        // memory adress location, if the content
+        // is equal, it return true
         for (int j = 0; keys[i] != null; i = (initial + (y * ++j)) % m) {
             if (keys[i].equals(k)) {
                 if (values[i] == null) {
@@ -108,7 +107,8 @@ public class OpenAddressingHashTable<Key, Value> {
     }
 
     public void put(Key k, Value v) {
-        if (k == null) return;
+        if (k == null)
+            return;
         if (v == null) {
             delete(k);
             return;
@@ -117,26 +117,29 @@ public class OpenAddressingHashTable<Key, Value> {
         int y = doubleHash(k);
         int i = initial;
 
-        // Is necessary create a new variable, to avoid the usage of previous result in increment on for loop:
+        // Is necessary create a new variable, to avoid the usage of previous result in
+        // increment on for loop:
         // x = x + (y*i) === m
         // x ( === m) x = m + (y*i(1)) ==== m -----> ERRO!
         // x = b + (y*i) === y*0m + b
         // x = b + (y*i(1) === (y*m(1)) + b
-        //avançar com o ponteiro i até um valor null dentro da hashTable, fazendo linearProbing
+        // avançar com o ponteiro i até um valor null dentro da hashTable, fazendo
+        // linearProbing
         for (int j = 0; keys[i] != null; i = (initial + (y * ++j)) % m) {
-            //check if key already exists
+            // check if key already exists
             if (keys[i].equals(k)) {
-                //se for inserida chave ja existente mas antes estava marcada como apagada (values[i] == null), será um reciclagem;
+                // se for inserida chave ja existente mas antes estava marcada como apagada
+                // (values[i] == null), será um reciclagem;
                 if (values[i] == null) {
                     deletedCounter--;
                     break;
                 }
-                //updating value
+                // updating value
                 values[i] = v;
                 return;
             }
         }
-        //once key index was found, insert it
+        // once key index was found, insert it
         keys[i] = k;
         values[i] = v;
         size++;
@@ -147,16 +150,18 @@ public class OpenAddressingHashTable<Key, Value> {
     }
 
     public void delete(Key k) {
-        if (k == null) return;
+        if (k == null)
+            return;
 
         int i = hash(k);
         int y = doubleHash(k);
         int initial = i;
 
         for (int j = 0; keys[i] != null; i = (initial + (y * ++j)) % m) {
-            //i index already positionated in exactely index that might be deleted
+            // i index already positionated in exactely index that might be deleted
             if (keys[i].equals(k)) {
-                if (values[i] == null) return;
+                if (values[i] == null)
+                    return;
                 values[i] = null;
                 deletedCounter++;
                 size--;
@@ -181,7 +186,6 @@ public class OpenAddressingHashTable<Key, Value> {
                     int currentIndex = 0;
                     int validKeys = 0;
 
-
                     @Override
                     public boolean hasNext() {
                         return validKeys < size;
@@ -189,8 +193,10 @@ public class OpenAddressingHashTable<Key, Value> {
 
                     @Override
                     public Key next() {
-                        while (hasNext() && values[currentIndex] == null) currentIndex++;
-                        if (!hasNext()) return null;
+                        while (hasNext() && values[currentIndex] == null)
+                            currentIndex++;
+                        if (!hasNext())
+                            return null;
                         validKeys++;
                         return keys[currentIndex++];
                     }
@@ -199,19 +205,21 @@ public class OpenAddressingHashTable<Key, Value> {
         };
     }
 
-    //-------------------- acessory methods ----------------------
+    // -------------------- acessory methods ----------------------
     private void resize(int primeIndex) {
-        //avoid errors
-        if (primeIndex < 0 || primeIndex >= primes.length) return;
+        // avoid errors
+        if (primeIndex < 0 || primeIndex >= primes.length)
+            return;
 
-        //new hashTable, using new prime as reference to capacity;
-        //Constructor(newSize) already update m value to next primeIndex
+        // new hashTable, using new prime as reference to capacity;
+        // Constructor(newSize) already update m value to next primeIndex
         OpenAddressingHashTable<Key, Value> aux = new OpenAddressingHashTable<>(primeIndex);
-        //placing all existing keys on new hashTable
+        // placing all existing keys on new hashTable
         for (int i = 0; i < m; i++) {
-            //reinserting only valid keys;
+            // reinserting only valid keys;
             // (values[i] != null) talvez funcione;
-            if (keys[i] != null && values[i] != null) aux.put(keys[i], values[i]);
+            if (keys[i] != null && values[i] != null)
+                aux.put(keys[i], values[i]);
         }
         this.keys = aux.keys;
         this.values = aux.values;
@@ -220,16 +228,19 @@ public class OpenAddressingHashTable<Key, Value> {
         this.loadFactor = (float) size / m;
     }
 
-    //  LinearProbing pode gerar um clustering, ou agrupamento. Isto é, criar aglomerados de keys, que resulta em baixa
-    //  eficiencia em outros methods, devido a uma busca linear prolongada
+    // LinearProbing pode gerar um clustering, ou agrupamento. Isto é, criar
+    // aglomerados de keys, que resulta em baixa
+    // eficiencia em outros methods, devido a uma busca linear prolongada
     public void deleteLinearProbing(Key k) {
-        //lazy delete
-        if (k == null) return;
+        // lazy delete
+        if (k == null)
+            return;
         int i = hash(k);
         for (; keys[i] != null; i = (i + 1) % m) {
-            //i index already positionated in exactely index that might be deleted
+            // i index already positionated in exactely index that might be deleted
             if (keys[i].equals(k)) {
-                if (values[i] == null) return;
+                if (values[i] == null)
+                    return;
                 values[i] = null;
                 deletedCounter++;
                 size--;
@@ -246,27 +257,30 @@ public class OpenAddressingHashTable<Key, Value> {
     }
 
     public void putLinearProbing(Key k, Value v) {
-        if (k == null) return;
+        if (k == null)
+            return;
         if (v == null) {
             delete(k);
             return;
         }
         int i = hash(k);
-        //avançar com o ponteiro i até um valor null dentro da hashTable, fazendo linearProbing
+        // avançar com o ponteiro i até um valor null dentro da hashTable, fazendo
+        // linearProbing
         for (; keys[i] != null; i = (i + 1) % m) {
-            //check if key already exists
+            // check if key already exists
             if (keys[i].equals(k)) {
-                //se for inserida chave ja existente mas antes estava marcada como apagada (values[i] == null), será um reciclagem;
+                // se for inserida chave ja existente mas antes estava marcada como apagada
+                // (values[i] == null), será um reciclagem;
                 if (values[i] == null) {
                     deletedCounter--;
                     break;
                 }
-                //updating value
+                // updating value
                 values[i] = v;
                 return;
             }
         }
-        //once key index was found, insert it
+        // once key index was found, insert it
         keys[i] = k;
         values[i] = v;
         size++;
@@ -278,10 +292,11 @@ public class OpenAddressingHashTable<Key, Value> {
 
     public Value getLinearProbing(Key k) {
         int i = hash(k);
-        //difference between == and .equals():
+        // difference between == and .equals():
         // == operator do an adress comparation, setted in object memory location;
-        //.equals() method compares the content. Even if the objects has different memory adress location, if the content
-        //is equal, it return true
+        // .equals() method compares the content. Even if the objects has different
+        // memory adress location, if the content
+        // is equal, it return true
         for (; keys[i] != null; i = (i + 1) % m) {
             if (keys[i].equals(k)) {
                 if (values[i] == null) {
@@ -294,48 +309,47 @@ public class OpenAddressingHashTable<Key, Value> {
     }
 }
 
-/*  ------CORNER SITUATION----
-    ->quando usar put, e encontrar uma tombstone, usar esta ao invés de continuar buscando uma key null; DONE
-    ->quando fizer resize, a tombstone não deve inserida novamente, então deve-se diminuir size e deletedCounter; DONE
-    ->se ao fazer delete de uma key o fator de carga seja < 0.125 (1/8), deve-se fazer resize para primeIndex--
-      que no entanto deve ser no minimo [37] (MIN_PRIMEINDEX = 1);DONE
-    ->ao fazer delete de uma key, caso deletedCounter/m >= 0.2, as tombstones devem ser efetivamente deletadas; DONE
-
-
-    for (int j = 0; keys[i] != null; i = (i + y*j++) % m) {
-
-    classe deleted
-        ponteiro para as tombstones;
-        method que apaga todas as tombstones;
-
----------------TESTER-------------------
-criar clusters gigantes com o put(LINEARPROBING) e em seguida realizar get(LINEARPROBING) dos keys inseridos;
-salvar o tempo levado para o último;
-
-usar array put(DOUBLEHASHING) e em seguida realizar get(DOUBLEHASHING) dos keys inseridos;
-salvar o tempo levado para o último;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*
+ * ------CORNER SITUATION---- ->quando usar put, e encontrar uma tombstone, usar
+ * esta ao invés de continuar buscando uma key null; DONE ->quando fizer resize,
+ * a tombstone não deve inserida novamente, então deve-se diminuir size e
+ * deletedCounter; DONE ->se ao fazer delete de uma key o fator de carga seja <
+ * 0.125 (1/8), deve-se fazer resize para primeIndex-- que no entanto deve ser
+ * no minimo [37] (MIN_PRIMEINDEX = 1);DONE ->ao fazer delete de uma key, caso
+ * deletedCounter/m >= 0.2, as tombstones devem ser efetivamente deletadas; DONE
+ *
+ *
+ * for (int j = 0; keys[i] != null; i = (i + y*j++) % m) {
+ *
+ * classe deleted ponteiro para as tombstones; method que apaga todas as
+ * tombstones;
+ *
+ * ---------------TESTER------------------- criar clusters gigantes com o
+ * put(LINEARPROBING) e em seguida realizar get(LINEARPROBING) dos keys
+ * inseridos; salvar o tempo levado para o último;
+ *
+ * usar array put(DOUBLEHASHING) e em seguida realizar get(DOUBLEHASHING) dos
+ * keys inseridos; salvar o tempo levado para o último;
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
-
-
